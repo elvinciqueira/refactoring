425ac16ac35b4dcb6805f98e8e203fbb9b448f44
elvinciqueira <elvinciqueira@gmail.com>
2020-10-08 22:22:26 -0300

    formatando espaços

--- invoices.js
+++ invoices.js
@@ -1,19 +1,18 @@
-  module.exports = 
-    {
-      customer: "BigCo",
-      performances: [
-        {
-          playID: "hamlet",
-          audience: 55
-        },
-        {
-          playID: "asLike",
-          audience: 35
-        },
-        {
-          playID: "othello",
-          audience: 40
-        }
-      ],
-    }
+  module.exports = {
+    customer: "BigCo",
+    performances: [
+      {
+        playID: "hamlet",
+        audience: 55
+      },
+      {
+        playID: "asLike",
+        audience: 35
+      },
+      {
+        playID: "othello",
+        audience: 40
+      }
+    ],
+}
   
