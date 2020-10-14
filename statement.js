d30903cf07d7a4a9c51c0bf4c511c73907727bc2
elvinciqueira <elvinciqueira@gmail.com>
2020-10-14 20:44:28 -0300

    mover função

--- statement.js
+++ statement.js
@@ -11,12 +11,42 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
+    result.amount = amountFor(result);
 
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
+  }
+
+  
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+
+        break;
+      case "comedy":
+        result = 30000;
+
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+
+        result += 300 * aPerformance.audience;
+      break;
+    default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+
+    return result;
   }
 }
 
@@ -25,7 +55,7 @@ function renderPlainText(data, plays) {
   
   for (let perf of data.performances) {
     result += `${perf.play.name}: 
-      ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n
+      ${usd(perf.amount / 100)} (${perf.audience} seats)\n
     `;
   }
 
@@ -37,7 +67,7 @@ function renderPlainText(data, plays) {
     let result = 0;
 
     for(let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
 
     return result;
@@ -66,34 +96,6 @@ function renderPlainText(data, plays) {
     result += Math.max(aPerformance.audience - 30, 0);
 
     if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-
-        break;
-      case "comedy":
-        result = 30000;
-
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-
-        result += 300 * aPerformance.audience;
-      break;
-    default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
 
     return result;
   }
