4023e48c858599bdef90c486cc62c6c42b5faf43
elvinciqueira <elvinciqueira@gmail.com>
2020-10-14 20:20:05 -0300

    obtendo cliente e passando para estrutura de dados intermediária

--- statement.js
+++ statement.js
@@ -3,12 +3,13 @@ const plays = require('./plays.js');
 
 function statement(invoice, plays) {
   const statementData = {};
+  statementData.costumer = invoice.customer;
 
   return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+  let result = `Statement for ${data.customer}\n`;
   
   for (let perf of invoice.performances) {
     result += `${playFor(perf).name}: 
