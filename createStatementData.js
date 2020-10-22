dd878aaebbc79906d32d22ca6fa4685b5bb61679
elvinciqueira <elvinciqueira@gmail.com>
2020-10-22 20:15:50 -0300

    internalizando função

--- createStatementData.js
+++ createStatementData.js
@@ -12,7 +12,7 @@ function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
   
     return result;
   }
@@ -79,7 +79,7 @@ class PerformanceCalculator {
     
     result += Math.max(this.performance.audience - 30, 0);
   
-    if ("comedy" === this.performance.play.type) {
+    if ("comedy" === this.play.type) {
       result += Math.floor(this.performance.audience / 5);
     }
   
