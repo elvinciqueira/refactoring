2384e990036d626e0f5578b7125f7eb1e80377ec
elvinciqueira <elvinciqueira@gmail.com>
2020-10-22 20:46:03 -0300

    fix

--- createStatementData.js
+++ createStatementData.js
@@ -82,7 +82,7 @@ class ComedyCalculator extends PerformanceCalculator {
   }
 
   get volumeCredits() {
-    return Math.floor(this.performance.audience / 5);
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 
